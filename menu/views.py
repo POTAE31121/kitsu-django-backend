4d6d183520f729bbfc8a99ca7959058351f56a62
POTAE31121 <tae-b-boy@hotmail.com>
2026-03-19 00:15:41 +0700

    Fix AdminOrderAllowedTransitionsView

--- menu/views.py
+++ menu/views.py
@@ -30,6 +30,7 @@ from .serializers import (
     MenuItemSerializer,
     OrderStatusSerializer,
     AdminOrderSerializer,
+    AdminOrderAllowedTransitionsViewSerializer,
     OrderSlipUploadSerializer,
     FinalOrderSubmissionSerializer,
 )
